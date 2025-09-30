0411c2c9ebb5c08c570e3497285d355fbf190aa3
Fahim550 <fahimrah1012023@gmail.com>
2025-10-01 02:09:09 +0600

    add sort and slice method

--- arrayMethod.js
+++ arrayMethod.js
@@ -40,3 +40,37 @@ console.log("everyNum2",everyNum2);
 const numbers7=[8,10,[4,[16,[18]]]]
 const flatNum=numbers7.flat(2)
 console.log("flatNum",flatNum);
+
+// sort()
+const names=["fahim","karim","jadu","modu","shukkur ali"]
+names.sort()
+console.log("names",names);
+
+const numbers8=[28,30,21,6,45]
+numbers8.sort()
+console.log("numbers8",numbers8);
+// output :numbers8 [ 21, 28, 30, 45, 6 ]
+
+const numbers9=[50,32,45,12,60]
+numbers9.sort((a, b)=> a - b )
+console.log("numbers9",numbers9);
+
+// sort object
+const user=[
+    {name:"fahim",age:21},
+    {name:"mustafijur",age:28},
+    {name:"imad",age:12},
+    {name:"rafid",age:7}
+]
+user.sort((a ,b)=> a.age - b.age)
+console.log("object sort",user);
+
+// slice()
+const numbers10=[10,15,20,25,30]
+const sliceNum1=numbers10.slice(1,3)
+const sliceNum2=numbers10.slice(0,4)
+console.log("sliceNum1",sliceNum1);
+console.log("sliceNum2",sliceNum2);
+console.log("numbers10",numbers10);
+
+// splice()
